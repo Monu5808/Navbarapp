c64973599a043f663e351eb3e0628dba3174fcbc
sschhavi <88490340+sschhavi@users.noreply.github.com>
2023-03-11 09:40:58 +0530

    11 mar off

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,15 @@
-import React from 'react';
+import React from 'react'
 import logo from '../images/logo.svg';
 import "./Navbar.css";
+ 
 
 
 const Navbar = () => {
   return (
-    <div className='header'>
-      <img src={logo} />
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-      </ul>
-
-      <button> Login / Register</button>
-      <button>Job Post</button>
+    <div>
+      
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
